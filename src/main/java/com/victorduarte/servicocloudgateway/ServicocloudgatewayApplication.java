8cd5f5ae2d3ee0f4718d4ad945c2c9c62a68ba99
Victor <vhduartelima@hotmail.com>
2025-04-21 23:34:04 -0300

    initialize Spring Boot project with Maven wrapper and basic gateway setup

---
+++ src/main/java/com/victorduarte/servicocloudgateway/ServicocloudgatewayApplication.java
@@ -0,0 +1,25 @@
+package com.victorduarte.servicocloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ServicocloudgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicocloudgatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator routes(RouteLocatorBuilder builder){
+		return builder
+				.routes()
+				.route(route -> route.path("/api/client/**").uri("lb://servicoclientes"))
+				.build();
+	}
+}
---
+++ src/test/java/com/victorduarte/servicocloudgateway/ServicocloudgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victorduarte.servicocloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicocloudgatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
